f4e23ac1c5f42a9fa96280bd5b459228411f76d4
Mia <daryne.benredjeb@gmail.com>
2022-03-02 16:51:42 +0100

    update hpp portefeuille

--- portefeuille.hpp
+++ portefeuille.hpp
@@ -5,7 +5,13 @@ class Portefeuille {
         int nba;
         int capacite;
         Actif* actifs;
+        void redimensionneTableauActifs(int nouvelleCapacite);
     public: 
+        Portefeuille(int = 1);
+        Portefeuille(const Portefeuille&);
+        ~Portefeuille();
+        void achatActif(string,int,double);
+        double venteActif(string,int,double);
 
     
     
