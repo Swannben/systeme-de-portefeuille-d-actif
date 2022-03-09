e1298a2a9d8ebb4abffd5f92b38d630784380f6b
Swann <swannbenz@gmail.com>
2022-03-09 14:19:51 +0100

    oui

--- portefeuille.cpp
+++ portefeuille.cpp
@@ -19,20 +19,21 @@ Portefeuille::~Portefeuille() {
 }
 void Portefeuille::achatActif(string nom,int quantite,double prix) {
     int i = 0;
-    while ( i < _capacite && _actifs[i].getNom()!=nom) {
+    while ( i < _nba && _actifs[i].getNom()!=nom) {
         i++;
     }
     if (_actifs[i].getNom() == nom) {
         _actifs[i].achat(quantite, prix);
     }
     else if (_nba!=_capacite) {
-        _nba++;
+        
         _actifs[_nba] = Actif(nom, quantite, prix);
+        _nba++;
     }
     else {
-        _capacite++;
+        redimensionneTableauActifs(_capacite * 2);
         _nba++;
-        _actifs[_nba] = Actif(nom, quantite, prix);
+        _actifs[_nba-1] = Actif(nom, quantite, prix);
     }
 }
 double Portefeuille::venteActif(string nom,int quantite,double prix) {
@@ -57,6 +58,7 @@ double Portefeuille::venteActif(string nom,int quantite,double prix) {
 void Portefeuille::redimensionneTableauActifs(int nouvCap) {
     Actif* newTab = new Actif[nouvCap];
     copy(&_actifs[0], &_actifs[min(_nba,nouvCap)-1], &newTab[0]);
+    _capacite = nouvCap;
     std::swap(_actifs, newTab);
-    delete(newTab);
+    delete(&newTab);
 }
